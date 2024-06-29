b11cc1ee61b4703b45acc65d9e9babf69069819b
AbaidullahRajpoot <abaidullah.gwl@gmail.com>
2024-06-29 11:43:32 +0500

    change base url

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000',
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = getState()?.auth?.accessToken;
       if (token) {
